47dd3fdaf061d422bd60c129240d1ddb626bbc8b
JBMods <77801124+jbmods-cyber@users.noreply.github.com>
2021-01-23 00:24:33 +0700

    Update APKMod.sh

--- APKMod.sh
+++ APKMod.sh
@@ -19,13 +19,13 @@ enda='\033[0m'
 echo
 echo $ku" ___________" $i"Version 1.0"
 echo $ku"▕╮╭┻┻╮╭┻┻╮╭▕╮╲"
-echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $me"Apk Hacking For Android"
+echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $me"APK MOD BY JB-MODS"
 echo $ku"▕╭┻┻┻┛┗┻┻┛   ╰▏"
 echo $ku"▕╰━━━┓┈┈┈╭╮▕╭╮▏" $pu"Author : JB Mods"
 echo $ku"▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏" $pu"Youtube: Bngst Story"
 echo $ku"▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏" $pu"Contact: 0857-8540-2747"
 echo
-echo $me"GUNAKAN DENGAN BAIK"
+echo $me"PILIH SALAH SATU LALU ENTER"
 echo
 sleep 3
 echo $pu"[================]"
@@ -37,56 +37,56 @@ echo $cy" ___________________________"
 echo $cy"|$pu SILAHKAN PILIH APK DIBAWAH|"
 echo $cy"|"
 echo $cy"|"
-echo $cy"| $pu 1) Whatsapp Beta"
-echo $cy"| $pu 2) JBWhatsApp v1.21"
-echo $cy"| $pu 3) JBWhatsApp v2.21"
-echo $cy"| $pu 4) JBWhatsApp v3.21"
-echo $cy"| $pu 5) Kinemaster Full Mod"
-echo $cy"| $pu 6) MT Manager VIP"
+echo $cy"| $pu 1) TEMA WHATSAPP"
+echo $cy"| $pu 2) JB-WhatsApp"
+echo $cy"| $pu 3) Kinemaster Full Mod"
+echo $cy"| $pu 4) MT Manager VIP"
+echo $cy"| $pu 5) Follow Instagram"
+echo $cy"| $pu 6) Subscribe YouTube"
 echo $cy"|"
 echo $cy"|________"
 read -p "#~Make Your Choice :" pil
 
 case $pil in
 
-1) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+1) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
 xdg-open https://jbmodsofficial.blogspot.com
 
 ;;
 
-2) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+2) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
-xdg-open https://jbmodsofficial.blogspot.com
+xdg-open https://biolinky.co/jbmods
 
 ;;
 
-3) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+3) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
-xdg-open https://jbmodsofficial.blogspot.com
+xdg-open https://biolinky.co/jbmods
 
 ;;
 
-4) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+4) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
-xdg-open https://jbmodsofficial.blogspot.com
+xdg-open https://biolinky.co/jbmods
 
 ;;
 
-5) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+5) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
-xdg-open https://jbmodsofficial.blogspot.com
+xdg-open https://www.instagram.com/bngst.story
 
 ;;
 
-6) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+6) echo $ku"SABAR, MASIH PROSES COKKK..."
 sleep 2
-xdg-open https://jbmodsofficial.blogspot.com
+xdg-open https://www.youtube.com/c/BngstStory
 
 ;;
 
 *) echo $pu"NICE TO MEET YOU!"
 sleep 2
-echo $pu"HOPE TO SEE YOU SOON!"
+echo $pu"THANK YOU BNGST!"
 sleep 2
 esac
