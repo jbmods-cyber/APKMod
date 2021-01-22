cc67deaf2d7f65e9228d24da875195e7628fe8b1
JBMods <77801124+jbmods-cyber@users.noreply.github.com>
2021-01-23 00:51:07 +0700

    Update APKMod.sh

--- APKMod.sh
+++ APKMod.sh
@@ -19,33 +19,33 @@ enda='\033[0m'
 echo
 echo $ku" ___________" $i"Version 1.0"
 echo $ku"▕╮╭┻┻╮╭┻┻╮╭▕╮╲"
-echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $me"APK MOD BY JB-MODS"
+echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $pu"APK MOD BY JB-MODS"
 echo $ku"▕╭┻┻┻┛┗┻┻┛   ╰▏"
 echo $ku"▕╰━━━┓┈┈┈╭╮▕╭╮▏" $pu"Author : JB Mods"
 echo $ku"▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏" $pu"Youtube: Bngst Story"
 echo $ku"▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏" $pu"Contact: 0857-8540-2747"
 echo
-echo $me"PILIH SALAH SATU LALU ENTER"
+echo $i"SEMOGA ANDA SUKA & BERMANFAAT"
 echo
 sleep 3
-echo $pu"[================]"
-echo $pu"[ $me Loading Cokk..."
-echo $pu"[================]"
+echo $pu"[======================]"
+echo $pu" $me Loading Cokk..."
+echo $pu"[======================]"
 sleep 4
 echo
-echo $cy" ___________________________"
-echo $cy"|$pu SILAHKAN PILIH APK DIBAWAH|"
-echo $cy"|"
-echo $cy"|"
-echo $cy"| $pu 1) TEMA WHATSAPP"
-echo $cy"| $pu 2) JB-WhatsApp"
-echo $cy"| $pu 3) Kinemaster Full Mod"
-echo $cy"| $pu 4) MT Manager VIP"
-echo $cy"| $pu 5) Follow Instagram"
-echo $cy"| $pu 6) Subscribe YouTube"
-echo $cy"|"
-echo $cy"|________"
-read -p "#~Make Your Choice :" pil
+echo $i" ___________________________"
+echo $i"|$pu SILAHKAN PILIH APK DIBAWAH|"
+echo $i"|"
+echo $i"|"
+echo $i"| $pu 1) TEMA WHATSAPP"
+echo $i"| $pu 2) JB-WhatsApp"
+echo $i"| $pu 3) Kinemaster Full Mod"
+echo $i"| $pu 4) MT Manager VIP"
+echo $i"| $pu 5) Follow Instagram"
+echo $i"| $pu 6) Subscribe YouTube"
+echo $i"|"
+echo $i"|________"
+read -p "Tulis Angka Yang Anda Pilih :" pil
 
 case $pil in
 
