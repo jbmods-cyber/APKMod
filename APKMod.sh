97e795bd5a4476deaa1f86010dace3a44573b34a
JBMods <77801124+jbmods-cyber@users.noreply.github.com>
2021-01-22 14:52:54 +0700

    Add files via upload

---
+++ APKMod.sh
@@ -0,0 +1,92 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo
+echo $ku" ___________" $i"Version 1.0"
+echo $ku"▕╮╭┻┻╮╭┻┻╮╭▕╮╲"
+echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $me"Apk Hacking For Android"
+echo $ku"▕╭┻┻┻┛┗┻┻┛   ╰▏"
+echo $ku"▕╰━━━┓┈┈┈╭╮▕╭╮▏" $pu"Author : JB Mods"
+echo $ku"▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏" $pu"Youtube: Bngst Story"
+echo $ku"▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏" $pu"Contact: 0857-8540-2747"
+echo
+echo $me"GUNAKAN DENGAN BAIK"
+echo
+sleep 3
+echo $pu"[================]"
+echo $pu"[ $me Wait..."
+echo $pu"[================]"
+sleep 4
+echo
+echo $cy" ___________________________"
+echo $cy"|$pu SILAHKAN PILIH APK DIBAWAH|"
+echo $cy"|"
+echo $cy"|"
+echo $cy"| $pu 1) Whatsapp Beta"
+echo $cy"| $pu 2) JBWhatsApp v1.21"
+echo $cy"| $pu 3) JBWhatsApp v2.21"
+echo $cy"| $pu 4) JBWhatsApp v3.21"
+echo $cy"| $pu 5) Kinemaster Full Mod"
+echo $cy"| $pu 6) MT Manager VIP""
+echo $cy"|"
+echo $cy"|________"
+read -p "#~Make Your Choice :" pil
+
+case $pil in
+
+1) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+2) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+3) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+4) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+5) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+6) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://jbmodsofficial.blogspot.com
+
+;;
+
+*) echo $pu"NICE TO MEET YOU!"
+sleep 2
+echo $pu"HOPE TO SEE YOU SOON!"
+sleep 2
+esac
---
+++ README.md
@@ -0,0 +1,11 @@
+# APKMods
+Apk Hacking for Android
+# Install
+# pkg update && pkg upgrade
+# pkg install git
+# pkg install bash
+
+# run
+# git clone https://github.com/jbmods-cyber/APKMod
+# cd APKMod
+# sh APKMod.sh
